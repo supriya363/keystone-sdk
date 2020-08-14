1417f65952d8d45221a012d3ec004fb494a63ff1
Supriya Suresh <44939296+supriya363@users.noreply.github.com>
2020-08-14 21:32:21 +0530

    Update init.sh
    Corrected wrong repo link for runtime

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,7 +18,7 @@ init_runtime() {
 
     case $1 in
       eyrie)
-        repo_url=https://github.com/NirjharRoy/keystone-runtime
+        repo_url=https://github.com/supriya363/keystone-runtime.git
         ;;
       sel4)
         repo_url=https://github.com/keystone-enclave/keystone-sel4
