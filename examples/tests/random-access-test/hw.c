69b6e71200e6dcf740da96fda75326a355d6f422
Mriganka <mrigankachakravarty82624@gmail.com>
2020-10-06 15:48:16 +0530

    documentation added

--- examples/tests/random-access-test/hw.c
+++ examples/tests/random-access-test/hw.c
@@ -14,7 +14,7 @@
 typedef struct Page
 {
   int values[1024];
-} Page;
+}Page;
 
 	
 void EAPP_ENTRY eapp_entry(){	
@@ -26,14 +26,22 @@ void EAPP_ENTRY eapp_entry(){
         char* failed = "FAILED!\0";//8
 	Page *dynamicpage = (Page*)malloc(MAX*sizeof(Page));
 	Page *staticpage = (Page*)malloc(MAX*sizeof(Page));
+	//Initialize the staticpage(actually dynamic, named so as work had progressed)
+	//staticpage and static page are copies of each other. Aim of this App is to check if after encountering page faults andmoving arounf victims and bringing them back, still renders the pages unchanged, as must be the case.
+	
+
+	//Initializing the static page, to detect if page is fresh.
 	for(i=0;i<MAX;i++){
 		staticpage[i].values[0]=-1;
 	}
+
+	//Aim of this loop is to generate a random permutation from 0 to MAX-1 and fill these indices with random data.
 	for(i=0;i<MAX;i++){
+		//generate random indices 
 		s_rand((unsigned long)&num, (unsigned long)sizeof(int));
 		num = num<0?(-1)*num:num;
 		num = num%MAX;
-	
+		//checks if this index was written earlier, if not write random data
 		if(staticpage[num].values[0]!=-1){
 			--i;
 		}
@@ -48,13 +56,16 @@ void EAPP_ENTRY eapp_entry(){
 		}
 	}
 	ocall_print_buffer(check,10);
+// Uncommenting the below lines should cause a failure
 //	staticpage[0].values[0]=0;  //THESE LINES ARE ADDEDD TO DELIBERATELY CAUSE A MATCHING FAILURE`
 //	dynamicpage[0].values[0]=1;
 	
+
+
+	// Sequntial access test
 	for(i=0;i<MAX;i++){
 		for(k=0;k<1024;k++){
-			//ocall_print_value(staticpage[i].values[k]);
-                        //ocall_print_value(dynamicpage[i].values[k]);
+			//This if should never execute if everything is Hunky Dory.
 			if(staticpage[i].values[k] != dynamicpage[i].values[k]){
 				ocall_print_buffer(failed,8);
 				ocall_print_buffer(failed,8);
@@ -65,16 +76,15 @@ void EAPP_ENTRY eapp_entry(){
 			}
 		}
 	}
-	
 	ocall_print_buffer(success,9);
+
+	// Random access test
 	char* progressmessage="WORK IN PROGRESS!\0";//18
 	char* finalmessage="COMPLETED!\0";//11
 	for (i=0;i<LOOP;i++){
 		s_rand((unsigned long)&num, (unsigned long)sizeof(int));
-		//ocall_print_value(num);
 		num= num<0? (-1)*num : num;
 		num=num%MAX;
-		//ocall_print_value(num);
 		for(k=0;k<1024;k++){
 			if(staticpage[num%MAX].values[k] != dynamicpage[num%MAX].values[k]){
 				ocall_print_buffer(failed,8);
@@ -83,6 +93,7 @@ void EAPP_ENTRY eapp_entry(){
 
 		}
 		if (i%1000==0){
+			//Heartbeat message
 			ocall_print_buffer(progressmessage,18);
 			ocall_print_value(i);
 		}
